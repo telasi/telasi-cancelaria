c654bd05bab9d9a5f9a6ce7e28703cbca59e254a
Dimitri Kurashvili <dimitri@mecniereba.org>
2011-05-22 17:52:28 +0400

    index management full

--- app/models/index.rb
+++ app/models/index.rb
@@ -1,3 +1,9 @@
 class Index < ActiveRecord::Base
   has_one :relation, :class_name => 'Index', :foreign_key => 'relation_id'
+  validates_presence_of :prefix, :description
+  validates_numericality_of :last_number
+
+  def to_s
+    prefix
+  end
 end
