7eb1c48d6a98cd2b59dc371e81b970188c9278ed
Dimitri Kurashvili <dimitri@c12.ge>
2012-05-30 15:36:39 +0400

    add/remove user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,4 +44,24 @@ class UsersController < ApplicationController
     redirect_to(users_url)
   end
 
+  ## USER <<=>> INDEX
+
+  def add_user_index
+    @user = User.find(params[:user_id])
+    if request.post?
+      @user_index = UserIndex.new(params[:user_index])
+      @user_index.user = User.find(params[:user_id])
+      redirect_to @user, notice: 'ინდექსი დამატებულია.' if @user_index.save
+    else
+      @user_index = UserIndex.new
+    end
+  end
+
+  def delete_user_index
+    @user_index = UserIndex.find(params[:id])
+    user = @user_index.user
+    @user_index.destroy
+    redirect_to user, notice: 'ინდექსი წაშლილია.'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 # -*- encoding : utf-8 -*-
 Canc::Application.routes.draw do
-
   resources :users
   resources :letters
   resources :employees
@@ -12,14 +11,17 @@ Canc::Application.routes.draw do
   match '/login', :controller => :site, :action => :login
   match '/logout', :controller => :site, :action => :logout
   match '/change_password', :controller => :site, :action => :change_password
-  
+
   match '/style', :controller => :style, :action => :style
   match '/add_department', :controller => :letters, :action => :add_department
   match '/remove_department', :controller => :letters, :action => :remove_department
   match '/add_employee', :controller => :letters, :action => :add_employee
   match '/remove_employee', :controller => :letters, :action => :remove_employee
   match '/search', :controller => :letters, :action => :search
-  
+
+  match '/add_user_index/:user_id', controller: :users, action: :add_user_index, as: :add_user_index
+  delete '/delete_user_index/:id', controller: :users, action: :delete_user_index, as: :delete_user_index
+
   match '/not_completed_letters', :controller => :reports, :action => :not_completed_letters
   match '/completed_letters_by_index', :controller => :reports, :action => :completed_letters_by_index
 
