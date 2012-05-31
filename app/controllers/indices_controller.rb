860aa58d130ae8de313476c5338afd21db642c11
Dimitri Kurashvili <dimitri@c12.ge>
2012-05-31 11:28:04 +0400

    simplify indices controller

--- app/controllers/indices_controller.rb
+++ app/controllers/indices_controller.rb
@@ -3,30 +3,15 @@ class IndicesController < ApplicationController
 
   def index
     @indices = Index.all(:order => :prefix)
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @indices }
-    end
   end
 
   def show
     @index = Index.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @index }
-    end
   end
 
   def new
     redirect_to(indices_url, :notice => 'არ გაქვთ ახალი ინდექსის დამატების უფლება!') and return if !get_current_user.can_edit_indices
     @index = Index.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @index }
-    end
   end
 
   def edit
@@ -36,29 +21,19 @@ class IndicesController < ApplicationController
 
   def create
     @index = Index.new(params[:index])
-
-    respond_to do |format|
-      if @index.save
-        format.html { redirect_to(@index, :notice => 'ინდექსი შექმნილია.') }
-        format.xml  { render :xml => @index, :status => :created, :location => @index }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @index.errors, :status => :unprocessable_entity }
-      end
+    if @index.save
+      redirect_to(@index, :notice => 'ინდექსი შექმნილია.')
+    else
+      render :action => "new"
     end
   end
 
   def update
     @index = Index.find(params[:id])
-
-    respond_to do |format|
-      if @index.update_attributes(params[:index])
-        format.html { redirect_to(@index, :notice => 'ინდექსი შეცვლილია.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @index.errors, :status => :unprocessable_entity }
-      end
+    if @index.update_attributes(params[:index])
+      redirect_to(@index, :notice => 'ინდექსი შეცვლილია.')
+    else
+      render :action => "edit"
     end
   end
 
@@ -69,10 +44,5 @@ class IndicesController < ApplicationController
     redirect_to(@index, :notice => 'ვერ წავშლი: ეს ინდექსი გამოყენებაშია!') and return if Letter.where(:index_id => @index.id).count > 0    
 
     @index.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(indices_url) }
-      format.xml  { head :ok }
-    end
   end
 end
