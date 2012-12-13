b8f0ecfd180861a1e18939322520a73d9719f4b5
Dimitri Kurashvili <dimitri@c12.ge>
2012-12-13 11:31:58 +0400

    decorate {{red text}} in letter description

--- app/helpers/letters_helper.rb
+++ app/helpers/letters_helper.rb
@@ -1,3 +1,12 @@
 # -*- encoding : utf-8 -*-
 module LettersHelper
+
+  def decorate_description(description)
+    if description
+      new_description = h(description)
+      new_description = new_description.gsub(/\{\{/, '<span style="color: red; font-weight: bold;">').gsub(/\}\}/, '</span>')
+      new_description.html_safe
+    end
+  end
+
 end
